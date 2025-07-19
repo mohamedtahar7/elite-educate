85d20abdebba6fd2d3ee7b1df48d3164fb686af1
mohamedtahar7 <taharatallahx7@gmail.com>
2025-07-19 13:21:39 +0100

    done...... hopefully

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,6 @@ import Gradient from "@/components/main/Gradient";
 import Hero from "@/components/main/Hero";
 import Image from "next/image";
 import { useEffect, useState } from "react";
-import SignedHero from "@/components/main/SignedHero";
-
 export default function Home() {
   return (
     <main className="h-screen">
